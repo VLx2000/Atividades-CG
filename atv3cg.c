c96bab3ee0bc7781ce8c11834e84830e3e6489c0
Lucas D <lucasdomingues2000@hotmail.com>
2021-10-05 18:21:00 -0300

    Implenta uma rotação de 45 graus experimental

--- atv3cg.c
+++ atv3cg.c
@@ -7,13 +7,14 @@
 // ./exemplo1
 
 #include "cg2d.h"
+#include <math.h>
 
 int main(int argc, char ** argv) {
   
   palette * palheta;
   bufferdevice * monitor;
   window * janela;
-  viewport * porta;
+  viewport *porta1 , *porta2;
   object * poligono1, * poligono2;
   
   SetWorld(-128, 128, 127, 127); // Define o tamanho do mundo  
@@ -27,28 +28,41 @@ int main(int argc, char ** argv) {
   SetColor(1,1,1,palheta);
   
   // cria dois objetos bidimensionais
-  poligono1 = CreateObject(5);  // polígono fechado com 5 vértices
+  poligono1 = CreateObject(5); 
+  poligono2 = CreateObject(5); // polígono fechado com 5 vértices
   
   // Insere as coordenadas dos pontos representados no SRU em cada objeto
   // O terceiro parâmetro será discutido em aula futura
   // O quarto parâmetro é o indice associado a lookup table (cor)
-  SetObject(SetPoint(-9.0,-8.0,1,1), poligono1);
-  SetObject(SetPoint(-7.0,-3.0,1,1), poligono1);
-  SetObject(SetPoint(-4.0,-4.0,1,1), poligono1);
-  SetObject(SetPoint(-3.0,-6.0,1,1), poligono1);
-  SetObject(SetPoint(-6.0,-9.0,1,1), poligono1);
+  SetObject(SetPoint(-15.0,-12.0,2,1), poligono1);
+  SetObject(SetPoint(-10.0,-5.0,2,1), poligono1);
+  SetObject(SetPoint(-9.0,-7.0,2,1), poligono1);
+  SetObject(SetPoint(-7.0,-10.0,2,1), poligono1);
+  SetObject(SetPoint(-9.0,-11.0,2,1), poligono1);
+  
+  for(int i = 0;i<5;i++){
+    float x = poligono1->points[i].x;
+    float y = poligono1->points[i].y;
+    float new_x = x*sin(45) - y*sin(45);
+    printf("%f\n",new_x);
+    float new_y = y*cos(45) + x*sin(45);
+    SetObject(SetPoint(new_x,new_y,2,2), poligono2);
+  }
+
   
   
   janela = CreateWindow(-10.0,-10.0,0.0,0.0); // cria uma janela de visualização (coordenadas no SRU)
  
-  porta = CreateViewPort(0, 0, 639, 479); // Cria uma viewport
+  porta1 = CreateViewPort(0, 0, 639, 479); // Cria uma viewport
+  porta2 = CreateViewPort(0,0,639,479);
   // no caso uma única saída para o dispositivo de visualização com 640x480 entradas
   
-  DrawObject(poligono1,janela,porta,monitor,1);
-
+  DrawObject(poligono1,janela,porta1,monitor,1);
+  DrawObject(poligono2,janela,porta2,monitor,2);
  
   Dump2X(monitor,palheta);
- 
+ free(poligono1);
+ free(poligono2);
   return 0;
   }
 
